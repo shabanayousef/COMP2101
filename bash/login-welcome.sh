5748a4cbf7c0af05c776a895be6bab0404bdcd22
shabanayousef <shabanayousef9@gmail.com>
2019-06-19 12:24:35 -0400

    edited.

--- bash/login-welcome.sh
+++ bash/login-welcome.sh
@@ -1,6 +1,5 @@
 #!/bin/#!/usr/bin/env bash
 #
-
 # This script produces the dynamic welcome message
 # it should look like
 #   Welcome to planet hostname, title name!
@@ -8,16 +7,13 @@
 # Improve this script by adding some time and day of the week information to the welcome message
 #   Use a format like this:
 #   It is HH:MM AM on weekday.
-
 ###############
 # Variables   #
 ###############
-
 title="Overlord"
 hostname=$(hostname)
 date=$(date +'%I:%M %p')
 weekday=$(date +%u)
-
 ###############
 # Main        #
 ###############
